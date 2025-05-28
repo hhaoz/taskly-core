fb156f0866087eb43e943c90478e3227ef8e1617
hhaoz <165776317+hhaoz@users.noreply.github.com>
2025-05-28 13:39:13 +0700

    Update app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,7 +16,8 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer())5
+      scsdnc
       .get('/')
       .expect(200)
       .expect('Hello World!');
